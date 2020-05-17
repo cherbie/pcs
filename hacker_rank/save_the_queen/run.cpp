17ed4468bf34da050909db6a0fc1d2b8a66c188c
Clayton <herbsca1@gmail.com>
2020-05-17 23:27:54 +0800

    forking test cases and communicating between parent and child using pipes

--- hacker_rank/save_the_queen/run.cpp
+++ hacker_rank/save_the_queen/run.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <map>
 #include <stdexcept>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define INPUT_DIR "testcases/input"
 #define OUTPUT_DIR "testcases/output"
@@ -20,7 +22,10 @@ using namespace std;
 int readDirContents(const char*, map<int, string>&, bool);
 string resolvePath(const char*, const char*);
 void freeMemory(map<int, string>&, map<int, string>&);
-int runTests(map<int, string>&, map<int, string>&);
+int run(map<int, string>&, map<int, string>&);
+int exec_test(ifstream&, ifstream&);
+void read_from_pipe(int&);
+void write_to_pipe(int&);
 
 int main(void) {
   map<int, string> inputPaths;
@@ -32,7 +37,7 @@ int main(void) {
     throw runtime_error("Could not process output file paths.");
   
   // run tests
-  if(runTests(inputPaths, outputPaths) != 0)
+  if(run(inputPaths, outputPaths) != 0)
     cerr << "Not all tests successful!" << endl;
   
   freeMemory(inputPaths, outputPaths);
@@ -43,7 +48,7 @@ int main(void) {
 /**
  * Procedural logic to start child process and execute test
  */
-int runTests(map<int, string>& inputPaths, map<int, string>& outputPaths) {
+int run(map<int, string>& inputPaths, map<int, string>& outputPaths) {
   for(map<int,string>::iterator it = inputPaths.begin(); it != inputPaths.end(); ++it) {
     cout << "Test No. " << it->first << endl;
     ifstream finput;
@@ -52,6 +57,10 @@ int runTests(map<int, string>& inputPaths, map<int, string>& outputPaths) {
     foutput.open(outputPaths[it->first], ifstream::in);
     char* ibuf = (char*) calloc(sizeof(char), 10);
     char* obuf = (char*) calloc(sizeof(char), 10);
+
+    if(exec_test(finput, foutput) == 1)
+	return 0; // return as this is the child process
+    
     finput.read(ibuf, 9);
     foutput.read(obuf, 9);
 
@@ -64,6 +73,67 @@ int runTests(map<int, string>& inputPaths, map<int, string>& outputPaths) {
   }
 
   return 0;
+}
+
+int exec_test(ifstream& test_input, ifstream& test_output) {
+  int* pipestreams = (int*) malloc(sizeof(int) * 2);
+
+  if(pipe(pipestreams) != 0)
+    throw runtime_error("Unable to communicate to child process using pipes.");
+  pid_t pid = fork();
+  switch(pid) {
+    case -1: { // fork error
+      throw runtime_error("Error spawning child process.");
+    }
+    case 0: { // child process
+      close(pipestreams[0]);
+      dup2(pipestreams[1], 1); // read output of the child process and place in pipe
+      //write_to_pipe(pipestreams[1]);
+      close(pipestreams[1]);
+
+      //char* args[] = {"."};
+      if(execlp("pwd", "pwd") != 0)
+        cerr << "Error executing child process" << endl;
+      return 1; // return 1 if you are the child process
+    }
+    default: { // parent process
+      // pipestreams[0] // contains the read end of the pipe
+      close(pipestreams[1]); // don't want to write;
+
+      cout << "parents waiting" << endl;
+      int status;
+      wait(&status);
+      
+      if(status == 0)
+        read_from_pipe(pipestreams[0]);
+
+      close(pipestreams[0]);
+    }
+  }
+
+  free(pipestreams);
+  return 0;
+}
+
+void read_from_pipe(int& fd) {
+  char* buf = (char*) calloc(sizeof(char), 20);
+
+  if(read(fd, buf, sizeof(char) * 18) < 0) {
+    cerr << "Error reading pipe." << endl;
+  }
+
+  buf[19] = '\n';
+  cout << buf << endl;
+  free(buf);
+}
+
+void write_to_pipe(int& fd) {
+  char* buf =  (char*) calloc(sizeof(char), 20);
+  buf = strncpy(buf, "Hello World", 19);
+  if(write(fd, buf, sizeof(char) * 19) < 0)
+    cerr << "Error writing to pipe" << endl;
+  
+  free(buf);
 }
 
 int readDirContents(const char* dirname, map<int, string>& contents, bool input) {
