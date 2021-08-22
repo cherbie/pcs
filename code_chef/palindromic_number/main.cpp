83c608ddb3b67596543555357e6e5389b331250c
Clayton Herbst <herbsca1@gmail.com>
2021-08-22 14:38:09 +0800

    PALNUM Fixed DFS iterator and lexographical ordering calculation

--- code_chef/palindromic_number/main.cpp
+++ code_chef/palindromic_number/main.cpp
@@ -39,18 +39,19 @@ private:
     NodeSPtr m_root;
     /** Lookup to nodes based on values **/
     std::array<std::vector<NodeWPtr>, 10> m_lookup;
+    unsigned int m_maxWeight;
 
 public:
     Tie() = default;
-    Tie(const std::string &s)
+    Tie(const std::string &s, unsigned int maxWeight)
     {
+        m_maxWeight = maxWeight;
         m_root = std::make_shared<Node>(Node{
-            -1,
-            0, // tier of 0
-            0,
-            nullptr,
-            std::vector<NodeSPtr>{}});
-        std::cerr << "Debugging" << std::endl;
+            -1,      // value
+            0,       // tier
+            0,       // weight
+            nullptr, // parent
+            std::vector<NodeSPtr>{} /** children */});
         m_lookup = std::array<std::vector<NodeWPtr>, 10>{
             std::vector<NodeWPtr>(),
             std::vector<NodeWPtr>(),
@@ -64,19 +65,20 @@ public:
             std::vector<NodeWPtr>()};
         NodeWPtr parent_wp = std::weak_ptr(m_root);
         auto temp_char = std::array<char, 2>{'\n', '\n'};
+        unsigned int tier = 1;
         for (const char &c : s)
         {
             NodeSPtr parent = parent_wp.lock();
             temp_char[0] = c;
             int value = atoi(temp_char.cbegin());
             if (value < 0)
-                std::cerr << "Incorrect value read: " << value;
+                exit(EXIT_FAILURE);
             Node node = Node{
-                value,
-                1,
-                0,
-                parent,
-                std::vector<NodeSPtr>{}};
+                value,  /** value */
+                tier++, /** tier */
+                0,      /** weight */
+                parent, /** parent */
+                std::vector<NodeSPtr>{} /** children */};
             NodeSPtr node_sp = std::make_shared<Node>(node);
             parent->children.push_back(node_sp);
             parent_wp = std::weak_ptr(node_sp);
@@ -96,54 +98,51 @@ public:
         std::vector<NodeWPtr> nodes = m_lookup.at(x);
         for (const auto &node_wp : nodes)
         {
-            std::cerr << x << "|" << y << "|";
             NodeSPtr node_sp = node_wp.lock();
-            std::cerr << "now|" << node_sp->value << "|";
             std::vector<NodeSPtr> parents = node_sp->parent->parent != nullptr ? node_sp->parent->parent->children
                                                                                : std::vector<NodeSPtr>{node_sp->parent};
             for (const NodeSPtr &parent : parents)
             {
                 NodeSPtr nnode_sp = std::make_shared<Node>(Node{
-                    y,
-                    node_sp->tier,
-                    parent->weight + w,
-                    parent,
-                    std::vector<NodeSPtr>(node_sp->children)});
+                    y,                   /** value */
+                    node_sp->tier,       /** tier */
+                    node_sp->weight + w, /** weight */
+                    parent,              /** parent */
+                    std::vector<NodeSPtr>(node_sp->children) /** children */});
                 parent->children.push_back(nnode_sp);
-                std::cerr << "parent|" << parent->value << "|";
                 m_lookup[y].push_back(std::weak_ptr<Node>(nnode_sp));
             }
-            std::cerr << std::endl;
         }
     }
 
     class PalindromicIterator
     {
     private:
+        using WeightedNode = std::pair<unsigned int /** accumulated weight */, Tie::Node /** window */>;
         std::vector<PalindromicNumber> m_values;
+        unsigned int m_maxWeight;
 
         /**
          * Recursive depth-first traversal of graph
          */
-        void dfs(const Node &window, std::stringstream &stream)
+        void dfs(const WeightedNode &&window, std::stringstream &stream)
         {
-            if (window.children.empty())
+            if (window.second.children.empty())
             {
                 std::string seq = stream.str();
-                if (!is_palindrome(seq))
+                if (!is_palindrome(seq) || window.first > m_maxWeight)
+                {
                     return;
-                m_values.push_back(std::make_tuple(seq, window.weight));
+                }
+                m_values.push_back(std::make_tuple(seq, window.first));
             }
             else
             {
-                for (const auto &child : window.children)
+                for (const auto &child : window.second.children)
                 {
-                    stream.seekg(window.tier, std::stringstream::beg); // input stream
-                    stream.ignore(std::numeric_limits<std::streamsize>::max());
-                    stream.seekp(window.tier); // output stream
-                    stream.clear();
+                    stream.seekp(child->tier - 1, std::stringstream::beg); // input stream
                     stream << child->value;
-                    dfs(*child, stream);
+                    dfs(WeightedNode{window.first + window.second.weight, *child}, stream);
                 }
             }
         }
@@ -153,7 +152,8 @@ public:
         PalindromicIterator(const Tie &graph)
         {
             auto ss = std::stringstream();
-            dfs(*graph.m_root, ss);
+            m_maxWeight = graph.m_maxWeight;
+            dfs(WeightedNode{graph.m_root->weight, *graph.m_root}, ss);
         }
         ~PalindromicIterator()
         {
@@ -167,6 +167,11 @@ public:
         std::vector<PalindromicNumber>::const_iterator cend() const noexcept
         {
             return m_values.cend();
+        }
+
+        std::size_t size() const noexcept
+        {
+            return m_values.size();
         }
     }; // class PalindromicIterator
 };     // class Tie
@@ -183,7 +188,7 @@ int main(int argc, char *argv[])
         std::cin >> n >> m >> k;
         std::string s; // starting string
         std::cin >> s;
-        Tie graph = Tie(s);
+        Tie graph = Tie(s, k);
         for (int i = 0; i < m; i++)
         {
             // operations to perform
@@ -191,20 +196,18 @@ int main(int argc, char *argv[])
             int y; // character to replace with
             int w; // "coint" cost of operation
             std::cin >> x >> y >> w;
-
-            std::cerr << "Insert: " << x << std::endl;
             graph.insert(x, y, w);
         }
         auto palindromic_it = Tie::PalindromicIterator(graph);
-        auto min_element = std::min_element(palindromic_it.cbegin(),
+        // lexigraphically largest element
+        auto max_element = std::max_element(palindromic_it.cbegin(),
                                             palindromic_it.cend(),
                                             [](const PalindromicNumber &l, const PalindromicNumber &r) -> bool
-                                            { return std::get<1>(l) < std::get<1>(r); });
-        if (min_element != palindromic_it.cend())
-            std::cout << std::get<0>(*min_element);
+                                            { return std::get<0>(l) < std::get<0>(r); });
+        if (max_element != palindromic_it.cend())
+            std::cout << std::get<0>(*max_element) << '\n';
         else
-            std::cout << -1;
-        std::cout << "\n";
+            std::cout << -1 << '\n';
         t--;
     }
 }
