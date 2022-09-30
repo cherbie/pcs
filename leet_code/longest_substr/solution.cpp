8937439e5666f748b14c063b93c4079d30fd0d48
Clayton Herbst <herbsca1@gmail.com>
2022-09-30 12:37:44 +1000

    brutefore solution

--- leet_code/longest_substr/solution.cpp
+++ leet_code/longest_substr/solution.cpp
@@ -1,15 +1,32 @@
 #include "bits/stdc++.h"
 
-int lengthOfLongestSubstring(const string &s)
+int lengthOfLongestSubstring(const std::string &s)
 {
     int max = 0;
     auto start_it = s.begin();
-    std::map<const std::string, int> mem; 
+    // O(n)
     while (start_it != s.end())
     {
         auto max_it = start_it;
-        while (max_it != s.end() && *max_it != *start_it
+        max_it++;
+        std::set<char> seen;
+        // O(n)
+        while (max_it != s.end())
+        {
+            // O(log(n))
+            if (seen.find(*max_it) != seen.end())
+            {
+                // already seen
+                max = std::max(max, (int)std::distance(start_it, max_it) - 1);
+                break;
+            }
+            // else
+            seen.insert(*max_it);
+            max_it++;
+        }
+        start_it++;
     }
+    return max;
 }
 
 int main(int argc, char** argv)
