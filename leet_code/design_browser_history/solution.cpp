02ca1e002f9d5d5c8a504c4ce20f31ba598aa9e7
Clayton Herbst <herbsca1@gmail.com>
2023-04-11 09:31:33 +0800

    fix: speed improvements

--- leet_code/design_browser_history/solution.cpp
+++ leet_code/design_browser_history/solution.cpp
@@ -7,45 +7,45 @@ namespace
     public:
         BrowserHistory(const std::string &homepage)
         {
-            m_backward_history.push(homepage);
+            m_history.push_back(homepage);
+            m_current = 0;
         }
 
         void visit(const std::string &url)
         {
-            m_backward_history.push(url);
-            m_forward_history = {};
+            m_history.resize(m_current + 2, url);
+            m_current++;
+            m_history[m_current] = url;
         }
 
         std::string back(int steps)
         {
-            for (int i = 0; i < steps && m_backward_history.size() > 1; i++)
-            {
-                std::string url = m_backward_history.top();
-                m_forward_history.push(url);
-                m_backward_history.pop();
-            }
-            return current();
+            int max_steps_back = std::min(steps, m_current);
+            m_current -= max_steps_back;
+            return top();
         }
 
         std::string forward(int steps)
         {
-            for (int i = 0; i < steps && !m_forward_history.empty(); i++)
-            {
-                std::string url = m_forward_history.top();
-                m_backward_history.push(url);
-                m_forward_history.pop();
-            }
-            return current();
+            m_current = std::min(steps + m_current, (int)m_history.size() - 1);
+            return top();
         }
 
-        std::string current()
+        std::string top()
         {
-            return m_backward_history.top();
+            return m_history.at(m_current);
         }
+
+#ifdef DEBUG
+        int index()
+        {
+            return m_current;
+        }
+#endif // ifdef DEBUG
 
     private:
-        std::stack<std::string> m_backward_history;
-        std::stack<std::string> m_forward_history;
+        std::vector<std::string> m_history;
+        int m_current = 0;
     };
 }
 
@@ -55,8 +55,13 @@ int main(int argc, char **argv)
     ::BrowserHistory history("/homepage");
     history.visit("/homepage/another");
     history.visit("/some/page/");
-    history.back(4);
-    history.forward(1);
-    std::cout << history.current() << std::endl;
+    std::cout << history.index() << " ";
+    std::cout << history.back(4) << " " << history.index() << " ";
+    history.visit("/youtube.com");
+    history.visit("/another/page");
+    history.visit("/what/the/hell");
+    std::cout << history.index() << history.back(5) << " ";
+    std::cout << history.forward(1) << " " << history.index() << " ";
+    std::cout << history.top() << std::endl;
 #endif // ifdef DEBUG
 }
