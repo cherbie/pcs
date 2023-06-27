1acf3cfce33ba79626874c9889b9ad96714b6a57
Clayton Herbst <herbsca1@gmail.com>
2023-06-27 22:14:45 +1000

    feat: solve two sum leetcode problem w/ python3

---
+++ leet_code/two_sum/solution.py
@@ -0,0 +1,42 @@
+from sys import stdin, stdout
+from typing import List, Tuple
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums = sorted([(idx, x) for idx, x in enumerate(nums)],
+                                key=lambda item: item[1])
+
+        for trail_idx in range(0, len(nums)-1):
+            for lead_idx in range(trail_idx+1, len(nums)):
+                two_term_sum = sorted_nums[trail_idx][1] + sorted_nums[lead_idx][1]
+                print(f'{trail_idx} {lead_idx} {two_term_sum}')
+                if two_term_sum == target:
+                    return [sorted_nums[trail_idx][0], sorted_nums[lead_idx][0]]
+                elif two_term_sum > target:
+                    # it ain't gonna get better ...
+                    break
+
+        assert False, 'assumption that a valid two integer sum exists'
+
+
+def parse_num_test_cases() -> int:
+    return int(stdin.readline().strip())
+
+def parse_test_case_input() -> Tuple[int, List[int]]:
+    target_input = int(stdin.readline().strip())
+    num_terms: [int] = list(map(int, stdin.readline().strip().split(' ')))
+    return target_input, num_terms
+
+
+def print_output(terms: List[int]):
+    stdout.write(','.join(map(str, terms)))
+    stdout.write('\n')
+
+if __name__ == '__main__':
+    s = Solution()
+    num_cases = parse_num_test_cases()
+    for _ in range(num_cases):
+        target, terms = parse_test_case_input()
+        result = s.twoSum(terms, target)
+        print_output(result)
+    
