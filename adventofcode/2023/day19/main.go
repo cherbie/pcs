a89164aed4462edc01489a37923e985d79c67551
Clayton Herbst <herbsca1@gmail.com>
2023-12-19 19:47:31 +1100

    fix: solve day19 part1 aoc2023

--- adventofcode/2023/day19/main.go
+++ adventofcode/2023/day19/main.go
@@ -25,6 +25,7 @@ var (
 const (
 	OUTCOME_ACCEPTED = "A"
 	OUTCOME_REJECTED = "R"
+	START_WORKFLOW   = "in"
 )
 
 type RuleSet struct {
@@ -58,7 +59,6 @@ func readInput() (workflows Workflows, parts []Part, err error) {
 		if matches := ruleSetNameRegex.FindAllStringSubmatch(line, -1); matches == nil {
 			return nil, nil, errors.New("error: could not match ruleset name regexp")
 		} else {
-			fmt.Println(matches[0], " | ", matches[0][1])
 			ruleSetId := matches[0][1]
 			ruleSetArrToken := matches[0][2]
 
@@ -68,7 +68,6 @@ func readInput() (workflows Workflows, parts []Part, err error) {
 					// "A" or "R"
 					rule.result = ruleSetToken
 				} else {
-					fmt.Println(ruleSetMatches[0])
 					rule.leftTerm = ruleSetMatches[0][1]
 					rule.operation = ruleSetMatches[0][2]
 					rule.result = ruleSetMatches[0][4]
@@ -90,12 +89,11 @@ func readInput() (workflows Workflows, parts []Part, err error) {
 		if matches := stateRegex.FindAllStringSubmatch(line, -1); matches == nil {
 			return nil, nil, errors.New("error: could not match ruleset name regexp")
 		} else {
-			fmt.Println(matches)
 			part := Part{}
-			part.x, _ = strconv.Atoi(matches[0][0])
-			part.m, _ = strconv.Atoi(matches[0][1])
-			part.a, _ = strconv.Atoi(matches[0][2])
-			part.s, _ = strconv.Atoi(matches[0][3])
+			part.x, _ = strconv.Atoi(matches[0][1])
+			part.m, _ = strconv.Atoi(matches[0][2])
+			part.a, _ = strconv.Atoi(matches[0][3])
+			part.s, _ = strconv.Atoi(matches[0][4])
 
 			if err != nil {
 				return nil, nil, err
@@ -112,13 +110,110 @@ func readInput() (workflows Workflows, parts []Part, err error) {
 	return workflows, parts, nil
 }
 
+func determineOutcome(part Part, workflows Workflows) string {
+	start := workflows[START_WORKFLOW]
+
+	workflow := make([]RuleSet, len(start))
+	copy(workflow, start)
+
+	for len(workflow) > 0 {
+		rule := workflow[0]
+		// fmt.Println("rule: ", rule, " workflow", workflow, " part: ", part)
+		nextWorkflowId := ""
+		if rule.operation == ">" {
+			switch rule.leftTerm {
+			case "x":
+				if part.x > rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "m":
+				if part.m > rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "a":
+				if part.a > rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "s":
+				if part.s > rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			}
+		} else if rule.operation == "<" {
+			switch rule.leftTerm {
+			case "x":
+				if part.x < rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "m":
+				if part.m < rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "a":
+				if part.a < rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			case "s":
+				if part.s < rule.rightTerm {
+					nextWorkflowId = rule.result
+				} else {
+					workflow = workflow[1:]
+					continue
+				}
+			}
+		} else if rule.operation == "" {
+			nextWorkflowId = rule.result
+		} else {
+			panic("rule set operation not recognised")
+		}
+
+		if nextWorkflowId == OUTCOME_ACCEPTED || nextWorkflowId == OUTCOME_REJECTED {
+			return nextWorkflowId
+		} else {
+			workflow = workflows[nextWorkflowId]
+		}
+	}
+	panic("never to reach here")
+}
+
 func part1() error {
 	workflows, parts, err := readInput()
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(workflows, " | ", parts)
+	sumOfRatings := int64(0)
+	for _, part := range parts {
+		switch determineOutcome(part, workflows) {
+		case OUTCOME_ACCEPTED:
+			{
+				fmt.Println("ACCEPTED:", part)
+				sumOfRatings += int64(part.x) + int64(part.m) + int64(part.a) + int64(part.s)
+			}
+		}
+	}
 
+	fmt.Println("Sum of part ratings: ", sumOfRatings)
 	return nil
 }
